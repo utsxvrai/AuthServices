a04e834f9ec15100a805643c3b936c5ff23e398f
Utsav Rai <utsavrai2115@gmail.com>
2024-11-15 10:05:29 +0530

    fixed the bug(duplicate keys creation)

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     InfoController : require('./info-controller'),
     UserController : require('./user-controller'),
+    
 
 }
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,48 +1,55 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 const bcrypt = require('bcrypt');
 
 const { SALT } = require('../config/server-config');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // Define association with Role model
       this.belongsToMany(models.Role, {
         through: 'User_Roles',
-      })
+      });
     }
   }
-  User.init({
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 100]
-      }
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
 
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 100],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      indexes: [
+        {
+          unique: true,
+          fields: ['email'],
+          name: 'unique_user_email', // Specify a unique name for the index
+        },
+      ],
+    }
+  );
+
+  // Encrypt password before saving
   User.beforeCreate((user) => {
     const encryptedPassword = bcrypt.hashSync(user.password, SALT);
     user.password = encryptedPassword;
   });
+
   return User;
 };
+
+
